890872de92636608b5eee1cb5ccc2bb3a1c0894a
LariQueiroz <larissa.mariz1a@gmail.com>
2025-09-22 15:06:16 -0300

    Add files via upload

---
+++ petshop/src/test/java/com/senaidev1A/petshop/PetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senaidev1A.petshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
